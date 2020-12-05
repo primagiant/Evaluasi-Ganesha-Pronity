42e3bb0ca0c1eeb9e5c413b18266a16ae60278f4
primagiant <kadekprimagiantmartadinata@gmail.com>
2020-12-05 18:34:23 +0800

    - Mengkonfigurasi style.css     - Stylling navigation bar     - menambahkan javascript untuk mejalankan navigation bar

--- js/script.js
+++ js/script.js
@@ -2,3 +2,25 @@
 $(window).on("load",function(){
     $('.preloader-container').fadeOut();
 })
+// MENU SHOW HIDEN
+const navMenu = document.getElementById("nav-menu"),
+    toggleMenu = document.getElementById("nav-toggle"),
+    closeMenu = document.getElementById("nav-close")
+
+// SHOW
+toggleMenu.addEventListener('click', function(){
+    navMenu.classList.toggle('show');
+})
+
+// HIDE
+closeMenu.addEventListener('click', function(){
+    navMenu.classList.toggle('show');
+})
+
+// REMOVE MENU
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction() {
+    navMenu.classList.remove('show');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
